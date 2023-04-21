c1b9658b67b54d437c526ac43c0a24ea51a75888
Hao Zhang （张浩）(FU-技术中心) <hzhang28@trip.com>
2023-04-21 10:28:43 +0800

    fix: base64

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
-
-import Layout from './features/layout';
-import 'antd/dist/reset.css';
-
+import Layout from "./features/layout";
+import md5 from './utils/md5'
 
 
+
+
+let empCode = 'TR802065'
+let code = 'Ds5IXR'
+console.log(md5.hexMD5(empCode + '&a.dl!b*?' + code))
 function App() {
   return (
     <div className="App">
-      <Layout/>
+      <Layout />
     </div>
   );
 }
